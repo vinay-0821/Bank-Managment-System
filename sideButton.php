d0ccf11c82d7a93ae22daffe272c14e5ebebea7d
UdayKiran112 <udaykiranuk1126@gmail.com>
2023-12-01 00:31:17 +0530

    sideButton changes

--- sideButton.php
+++ sideButton.php
@@ -1,5 +1,5 @@
 <form class="form-inline my-2 my-lg-0">
-        <a href="" class="btn btn-outline-success" data-toggle="tooltip" title="Your current Account Balance">Acount Balance : Rs.<?php echo $userData['balance']; ?></a>  
+        <a href="" class="btn btn-outline-success" data-toggle="tooltip" title="Your current Account Balance">Account Balance : Rs.<?php echo $userData['balance']; ?></a>  
         <a href="accounts.php" data-toggle="tooltip" title="Account Summary" class="btn btn-outline-primary mx-1" ><i class="fa fa-book fa-fw"></i></a> 
         <a href="notice.php" data-toggle="tooltip" title="View Notice" class="btn btn-outline-primary mx-1" ><i class="fa fa-envelope fa-fw"></i></a> 
         <a href="feedback.php" data-toggle="tooltip" title="Help?" class="btn btn-outline-primary mx-1" ><i class="fa fa-question fa-fw"></i></a> 
